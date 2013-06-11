2c02d5aabac0c45f786ac3bd415e7ef519f7b7b3
Steven Nunez <snunez@cyrusinnovation.com>
2013-06-11 19:49:43 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeshopReviewApp::Application.config.secret_key_base = '7a0b673f9ee21f60b7c28bac41cfe54500602c7310737749dad03725f9955ca9f2e1f0ce5382dfb54489af984e90d41ad176eba2bffeb5513ec940d4624f8fea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeshopReviewApp::Application.config.session_store :cookie_store, key: '_coffeeshop_review_app_session'
